ac28ad9bc3fa56beb409376802d01e4e154330c4
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-06-20 11:01:07 +0200

    kompiluje się, gradle taki jak ma być, da się zainstalować, tylko się wywala - Caused by: java.lang.InstantiationException: java.lang.Class<pl.edu.pbs.pumjetnote.screen.NoteViewModel> has no zero argument constructor

--- app/src/main/java/pl/edu/pbs/pumjetnote/MainActivity.kt
+++ app/src/main/java/pl/edu/pbs/pumjetnote/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
                     val noteViewModel = viewModel<NoteViewModel>()
                     NotesApp(noteViewModel)
